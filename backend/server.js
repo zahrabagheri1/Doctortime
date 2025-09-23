2ae089dad126b60ed1b719e6db94068935ebdc20
zahrabagheri1 <zahrabagheriuk@gmail.com>
2025-09-23 21:08:36 +0330

    feat(setup/backend): install packes to backend file

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from './config/mongodb.js'
+
+
+// app config
+const app = express()
+const port = process.env.PORT || 4000
+connectDB()
+
+// middlewares
+app.use(express.json())
+app.use(cors())
+
+
+// api endpoints
+app.get('/', (req, res) => {
+    res.send("API WORKING")
+
+})
+
+app.listen(port, () => console.log("Server Started", port))
+
+
+
