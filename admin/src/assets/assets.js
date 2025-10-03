2104b092cb09f3e0514bec864a6203f23f021306
zahrabagheri1 <zahrabagheriuk@gmail.com>
2025-10-04 01:51:28 +0330

    feat(admin): add some icons and compliteassets file

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -1,8 +1,35 @@
 import doctimelogo from './imgs/doctimelogo.png'
 import doctimelogotype from './imgs/doctimelogologotype.png'
+import addicon from './imgs/icons/add-icon.png'
+import appointmenticon from './imgs/icons/appointment-icon.png'
+import appointmentsicon from './imgs/icons/appointments-icon.png'
+import canceleicon from './imgs/icons/cancele-icon.png'
+import doctoricon from './imgs/icons/doctor-icon.png'
+import doctoraddicon from './imgs/icons/doctoradd-icon.png'
+import earningicon from './imgs/icons/earning-icon.png'
+import listicon from './imgs/icons/list-icon.png'
+import patienticon from './imgs/icons/patient-icon.png'
+import tikicon from './imgs/icons/tick-icon.png'
+import uploadicon from './imgs/icons/upload-icon.png'
+import uploadareaicon from './imgs/icons/uploadarea-icon.png'
+import usericon from './imgs/icons/user-icon.png'
 
 
 export const assets = {
     doctimelogo,
-    doctimelogotype
+    doctimelogotype,
+    addicon,
+    appointmenticon,
+    appointmentsicon,
+    canceleicon,
+    doctoricon,
+    doctoraddicon,
+    earningicon,
+    listicon,
+    patienticon,
+    tikicon,
+    uploadareaicon,
+    uploadicon,
+    usericon
+
 }
