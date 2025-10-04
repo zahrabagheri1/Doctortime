bde81d0d48611f5452686c34da864da5732f0e96
zahrabagheri1 <zahrabagheriuk@gmail.com>
2025-10-04 22:18:44 +0330

    feat(admin): add sidebar

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -3,14 +3,30 @@ import Login from "./pages/Login";
 import { ToastContainer, toast } from "react-toastify";
 import { AdminContext } from "./context/AdminContext";
 import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Admin/Dashboard";
+import AllAppointment from "./pages/Admin/AllAppointment";
+import DoctorsList from "./pages/Admin/DoctorsList";
+import AddDoctor from "./pages/Admin/AddDoctor";
 
 const App = () => {
   const { aToken } = useContext(AdminContext);
 
   return aToken ? (
-    <div className="bg-white">
+    <div className="bg-[#f5f5f5]">
       <ToastContainer />
       <Navbar />
+      <div className="flex items-start">
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<></>} />
+          <Route path="/admin-dashboard" element={<Dashboard />} />
+          <Route path="/all-appointments" element={<AllAppointment />} />
+          <Route path="/add-doctor" element={<AddDoctor />} />
+          <Route path="/doctor-list" element={<DoctorsList />} />
+        </Routes>
+      </div>
     </div>
   ) : (
     <>
